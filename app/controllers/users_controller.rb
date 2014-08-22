ce4ec551477e15bf2890796a6d1c9c246d949da7
PashaP <pasha5231@gmail.com>
2014-08-22 11:58:25 +0000

    save master version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,69 @@
 class UsersController < ApplicationController
-def new
-    @user = User.new
+  
+  before_action :signed_in_user, only: [:edit, :update, :index]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
-  def show
+  
+  def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Данные успешно обновлены!"
+      redirect_to @user
+    else
+      render 'edit'
+    end    
+  end
+  
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to Twitter!"
       redirect_to @user
-      # Handle a successful save.
     else
       render 'new'
     end
   end
-
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Пользователь удален."
+    redirect_to users_url
+  end
+  
+  
   private
+  
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
+
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  
 end
